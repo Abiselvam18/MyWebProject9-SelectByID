edd260a0e3ab683dede09c9e6f7bf915ba2c9af5
Abinaya <abiselvam080@gmail.com>
2022-05-08 16:26:28 +0530

    commit 73

---
+++ MyWebProject9-SelectById/src/com/edu/DbConnect.java
@@ -0,0 +1,26 @@
+package com.edu;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DbConnect {
+	//Connection with database
+			static String driver="com.mysql.cj.jdbc.Driver";
+			static String url="jdbc:mysql://localhost:3307/SpringDb";
+			static String un="root";
+			static String pswd="root";
+			static Connection con;
+			
+			public static Connection getConnection() {
+				try {
+					Class.forName(driver);
+					con=DriverManager.getConnection(url, un, pswd);
+					if(con==null) {
+						System.out.println("Connection is null");
+					}
+				}catch(Exception e) {
+					e.printStackTrace();
+				}
+				return con;
+			}
+}
